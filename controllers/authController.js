54c80a687bf53011b2f201414b2caad4940bfcdf
Joshua Abu <joshujah007@gmail.com>
2017-07-02 00:25:16 -0400

    sign up and sign in api created; need to expand passport strategy to include username

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,27 +1,48 @@
 
 module.exports = function(app, passport){
-  
+
   // process the signup form
   app.post('/api/signup', function(req, res, next) {
-    // console.log('req', req);
-    // console.log('res', res);
+
     passport.authenticate('local-signup', function(err, user, info) {
-      console.log('user', user);
+
       if (err) {
         return next(err); // will generate a 500 error
       }
-      console.log('info', info);
 
       // Generate a JSON response reflecting authentication status
       if (! user) {
-        return res.send(401,{ success : false, message : 'authentication failed' });
+        return res.send(401, { success : false, message : info.message });
       }
       req.login(user, function(err){
         if(err){
           return next(err);
         }
-        return res.send({ success : true, message : 'authentication succeeded' });
+        return res.send({ success : true, message : 'New User created' });
       });
     })(req, res, next);
   });
+
+  // process the login form
+  app.post('/api/signin', function(req, res, next) {
+
+    passport.authenticate('local-login', function(err, user, info) {
+      if (err) {
+        return next(err); // will generate a 500 error
+      }
+
+      // Generate a JSON response reflecting authentication status
+      if (! user) {
+        return res.send(401, { success : false, message : info.message });
+      }
+      req.login(user, function(err){
+        if(err){
+          return next(err);
+        }
+        return res.send({ success : true, message : 'Authenticated' });
+      });
+    })(req, res, next);
+  });
+
+
 }
